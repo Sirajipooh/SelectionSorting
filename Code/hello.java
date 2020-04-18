b4e75e3510911395ab51233e473e4875a1c1f9bd
Siraj <siraj@Susils-MacBook-Air.local>
2020-04-18 11:29:09 -0700

    h

--- Code/hello.java
+++ Code/hello.java
@@ -1,10 +1,10 @@
-class hello
+class Hello
 
 
 {
     public static void main(String[] args) 
     {
-        String bob = "hello1";
+        String bob = "hello12";
         System.out.println(bob);
     } 
 }
